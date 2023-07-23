73064abdca3790c86b8558fe2c568f79401025a3
DESKTOP-348BOL7\202208-2 <sasajimak@ubint.jp>
2023-07-23 18:09:10 +0900

    モデルとシリアライザーの作成

--- backend/runcalc/admin.py
+++ backend/runcalc/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Admin, User, Specialty
 
 # Register your models here.
+admin.site.register(Admin)
+admin.site.register(User)
+admin.site.register(Specialty)
